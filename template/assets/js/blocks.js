3b1fef4034bf07cdf626728d299f79c10fa893d6
sosenx <barteksosnowski711@gmail.com>
2017-10-26 18:50:11 +0200

    a

--- template/assets/js/blocks.js
+++ template/assets/js/blocks.js
@@ -343,6 +343,37 @@
 		});
 	});
 })();
+// Block name: Tabs
+// Dependencies: jquery.easytabs.js, velocity.js
+// Docs: 
+// https://github.com/JangoSteve/jQuery-EasyTabs
+// https://github.com/julianshapiro/velocity
+(function(){
+	$('.js-tabs').each(function() {
+		var tabs = $(this);
+
+		tabs.easytabs({
+			tabActiveClass: "tab__title--active",
+			updateHash: false
+		});
+
+		tabs.on("easytabs:before", function () {
+			$(this).find('.tabs__content.active img').velocity("stop").velocity("transition.slideDownOut", {
+				duration: 1500,
+				display: null
+			});
+		});
+
+		tabs.on("easytabs:midTransition", function (event, $clicked, $targetPanel) {
+			$targetPanel.find('img').velocity("stop").velocity("transition.slideDownIn", {
+				duration: 1500,
+				display: null,
+				stagger: 100
+			});
+		});
+	});
+
+})();
 // Block name: Testimonials
 // Dependencies: jquery.easytabs.js, velocity.js
 // Docs: 
@@ -371,37 +402,6 @@
 			$targetPanel.find("span").velocity("stop").velocity("transition.slideDownIn", {
 				duration: 1000,
 				display: null
-			});
-		});
-	});
-
-})();
-// Block name: Tabs
-// Dependencies: jquery.easytabs.js, velocity.js
-// Docs: 
-// https://github.com/JangoSteve/jQuery-EasyTabs
-// https://github.com/julianshapiro/velocity
-(function(){
-	$('.js-tabs').each(function() {
-		var tabs = $(this);
-
-		tabs.easytabs({
-			tabActiveClass: "tab__title--active",
-			updateHash: false
-		});
-
-		tabs.on("easytabs:before", function () {
-			$(this).find('.tabs__content.active img').velocity("stop").velocity("transition.slideDownOut", {
-				duration: 1500,
-				display: null
-			});
-		});
-
-		tabs.on("easytabs:midTransition", function (event, $clicked, $targetPanel) {
-			$targetPanel.find('img').velocity("stop").velocity("transition.slideDownIn", {
-				duration: 1500,
-				display: null,
-				stagger: 100
 			});
 		});
 	});
--- template/dist/script.js
+++ template/dist/script.js
@@ -22749,6 +22749,37 @@ return function (global, window, document, undefined) {
 		});
 	});
 })();
+// Block name: Tabs
+// Dependencies: jquery.easytabs.js, velocity.js
+// Docs: 
+// https://github.com/JangoSteve/jQuery-EasyTabs
+// https://github.com/julianshapiro/velocity
+(function(){
+	$('.js-tabs').each(function() {
+		var tabs = $(this);
+
+		tabs.easytabs({
+			tabActiveClass: "tab__title--active",
+			updateHash: false
+		});
+
+		tabs.on("easytabs:before", function () {
+			$(this).find('.tabs__content.active img').velocity("stop").velocity("transition.slideDownOut", {
+				duration: 1500,
+				display: null
+			});
+		});
+
+		tabs.on("easytabs:midTransition", function (event, $clicked, $targetPanel) {
+			$targetPanel.find('img').velocity("stop").velocity("transition.slideDownIn", {
+				duration: 1500,
+				display: null,
+				stagger: 100
+			});
+		});
+	});
+
+})();
 // Block name: Testimonials
 // Dependencies: jquery.easytabs.js, velocity.js
 // Docs: 
@@ -22777,37 +22808,6 @@ return function (global, window, document, undefined) {
 			$targetPanel.find("span").velocity("stop").velocity("transition.slideDownIn", {
 				duration: 1000,
 				display: null
-			});
-		});
-	});
-
-})();
-// Block name: Tabs
-// Dependencies: jquery.easytabs.js, velocity.js
-// Docs: 
-// https://github.com/JangoSteve/jQuery-EasyTabs
-// https://github.com/julianshapiro/velocity
-(function(){
-	$('.js-tabs').each(function() {
-		var tabs = $(this);
-
-		tabs.easytabs({
-			tabActiveClass: "tab__title--active",
-			updateHash: false
-		});
-
-		tabs.on("easytabs:before", function () {
-			$(this).find('.tabs__content.active img').velocity("stop").velocity("transition.slideDownOut", {
-				duration: 1500,
-				display: null
-			});
-		});
-
-		tabs.on("easytabs:midTransition", function (event, $clicked, $targetPanel) {
-			$targetPanel.find('img').velocity("stop").velocity("transition.slideDownIn", {
-				duration: 1500,
-				display: null,
-				stagger: 100
 			});
 		});
 	});
