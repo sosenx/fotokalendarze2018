de8ed6fc45c804be04a1a4a910de0b1ffa56fa85
sosenx <barteksosnowski711@gmail.com>
2017-10-28 01:07:55 +0200

    handlowcy

--- template/dist/script.js
+++ template/dist/script.js
@@ -22525,34 +22525,6 @@ return function (global, window, document, undefined) {
 		}
 	})();
 })();
-// Block name: Header
-// Dependencies: no dependencies
-// Docs: 
-(function(){
-	var header = $(".js-header").first();
-	var logo = header.find('.js-logo__image');
-
-	var updateHeader = function() {
-		if($(window).scrollTop() === 0){
-			header.removeClass('header--fixed');
-			if (logo.data('switch') === true) {
-				logo.attr("src","assets/img/common/logo/logo.png");
-			}
-		}else{
-			header.addClass('header--fixed');
-			if (logo.data('switch') === true) {
-				logo.attr("src","assets/img/common/logo/logo-dark.png");
-			}
-		}
-	};
-
-	if (header.length) { updateHeader(); }
-
-	$(window).scroll(function () {
-		updateHeader();
-	});
-
-})();
 // Block name: Map
 // Dependencies: Google Maps API
 // Docs: https://developers.google.com/maps/
@@ -22872,6 +22844,34 @@ return function (global, window, document, undefined) {
 // Docs: https://github.com/QassimHassan/YouTube_PopUp
 (function(){
 	$(".js-video").YouTubePopUp();
+})();
+// Block name: Header
+// Dependencies: no dependencies
+// Docs: 
+(function(){
+	var header = $(".js-header").first();
+	var logo = header.find('.js-logo__image');
+
+	var updateHeader = function() {
+		if($(window).scrollTop() === 0){
+			header.removeClass('header--fixed');
+			if (logo.data('switch') === true) {
+				logo.attr("src","assets/img/common/logo/logo.png");
+			}
+		}else{
+			header.addClass('header--fixed');
+			if (logo.data('switch') === true) {
+				logo.attr("src","assets/img/common/logo/logo-dark.png");
+			}
+		}
+	};
+
+	if (header.length) { updateHeader(); }
+
+	$(window).scroll(function () {
+		updateHeader();
+	});
+
 })();
 	
 })(jQuery);
