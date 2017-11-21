4fd52a6543e436989939329b8487df7690451b9c
barteksosnowski711@gmail.com <you@example.com>
2017-11-21 14:48:44 +0100

    wersja responsywna sidebarów

--- template/assets/js/blocks.js
+++ template/assets/js/blocks.js
@@ -86,31 +86,6 @@
 		});
 	});
 })();
-// Block name: Carousel
-// Dependencies: owl.carousel.js
-// Docs: https://github.com/OwlCarousel2/OwlCarousel2
-(function(){
-	$(".js-carousel").each(function() {
-		var carousel = $(this);
-
-		carousel.on('initialized.owl.carousel', function() {
-			$(this).find('.owl-item.active:first, .owl-item.active:last').addClass('has-fade');
-		}).owlCarousel({
-			loop:true,
-			responsive: {
-				0:    { items: 3 },
-				768:  { items: 4 },
-				991:  { items: 5 },
-				1200: { items: 7 }
-			}
-		}).on("drag.owl.carousel", function() {
-			$(this).find('.has-fade').removeClass('has-fade');
-		}).on("dragged.owl.carousel", function() {
-			$(this).find('.owl-item.active:first, .owl-item.active:last').addClass('has-fade');
-		});
-	});
-
-})();
 // Block name: Contact Form
 // Dependencies: jquery.form-validator.js
 // Docs: https://github.com/victorjonsson/jQuery-Form-Validator
@@ -150,6 +125,31 @@
 			});
 		}
 	})();
+})();
+// Block name: Carousel
+// Dependencies: owl.carousel.js
+// Docs: https://github.com/OwlCarousel2/OwlCarousel2
+(function(){
+	$(".js-carousel").each(function() {
+		var carousel = $(this);
+
+		carousel.on('initialized.owl.carousel', function() {
+			$(this).find('.owl-item.active:first, .owl-item.active:last').addClass('has-fade');
+		}).owlCarousel({
+			loop:true,
+			responsive: {
+				0:    { items: 3 },
+				768:  { items: 4 },
+				991:  { items: 5 },
+				1200: { items: 7 }
+			}
+		}).on("drag.owl.carousel", function() {
+			$(this).find('.has-fade').removeClass('has-fade');
+		}).on("dragged.owl.carousel", function() {
+			$(this).find('.owl-item.active:first, .owl-item.active:last').addClass('has-fade');
+		});
+	});
+
 })();
 // Block name: Header
 // Dependencies: no dependencies
